43719415eb897c01b1fb35e9148521a8a4a951ed
kilesh maharjan <kileshmhrzn@gmail.com>
2024-10-04 18:03:43 +0545

    fix: remove unused code

--- src/services/customer.service.tsx
+++ src/services/customer.service.tsx
@@ -49,27 +49,6 @@ class CustomerService {
       throw new Error("Error fetching data");
     }
   }
-  // async getCustomer<T>(id: number): Promise<T> {
-  //   try {
-  //     const response = await fetch(`/${this.baseUrl}`, {
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         Accept: "application/json",
-  //       },
-  //     });
-  //     const data = await response.json();
-  //     const customerDetail = data.filter(
-  //       (customer: User) => customer.id === id
-  //     );
-  //     return customerDetail[0];
-  //   } catch (error) {
-  //     console.log(
-  //       "🚀 ~ file: customer.service.tsx:28 ~ CustomerService ~ error:",
-  //       error
-  //     );
-  //     throw new Error("Error fetching data");
-  //   }
-  // }
 }
 
 export default CustomerService;
